43e2337ad4893327b1cc4f7e223d80193d3c8b61
Fabricio Costa <fabricio.costa@moldeointeractive.com.ar>
2018-10-12 11:39:41 -0300

    Added l10n_ar_bank

---
+++ OTools/l10n_ar/l10n_ar_bank/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Listado de Bancos Argentinos",
+    'version': '10.0.1.0.0',
+    'category': 'Localization/Argentina',
+    'sequence': 14,
+    'author': 'ADHOC SA,Moldeo Interactive,Odoo Community Association (OCA)',
+    'license': 'AGPL-3',
+    'summary': '',
+    'depends': [
+        'base',
+    ],
+    'external_dependencies': {
+    },
+    'data': [
+        'data/res_bank.xml',
+        'l10n_ar_bank.xml',
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'images': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
